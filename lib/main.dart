2a3d6129086a5d35adf9da258c41b7195693945b
tariq_anwer <tariqanwer2009@gmail.com>
2024-09-13 09:31:57 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,57 @@
-import 'package:flutter/material.dart';
+// Question 1
+void main() {
+  // Create a map named "car"
+  Map<String, dynamic> car = {
+    "brand": "Toyota",
+    "color": "Red",
+    "isSedan": true
+  };
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  // Check if the car is a sedan and red in color
+  if (car["isSedan"] == true && car["color"] == "Red") {
+    print("Match");
+  } else {
+    print("No match");
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+// Question 2
+void main() {
+  Map<String, dynamic> car = {
+    "Name": "Mubashir",
+    "is Admin": "true",
+    "is Active ": true
+  };
+  if (user["is Active"] == true && user["is Admin"] == "true") {
+    print("Active Admin");
+  } else {
+    print("Not Active Admin");
   }
+}
+
+// Question 3
+void main() {
+//themultiplication table of a given number using a for loop.
+  for (var i = 1; i <= 10; i++) {
+    print(i * 5);
+  }
+}
+
+//Question  4
+void main() {
+  // Create a list of numbers
+  List<int> numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+
+  // Initialize the largest number with the first element
+  int largest = numbers[0];
+
+  // Iterate through the list to find the largest number
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+
+  // Print the largest number
+  print("Largest element: $largest");
 }
